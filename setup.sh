2f5096a92cc70fc7a1fe3263b598fbb5905aaa1c
Tundra Fizz <coleman.leif@gmail.com>
2018-07-03 14:25:15 -0700

    Add setup.sh

---
+++ setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Create a new directory
+mkdir a && cd a
+
+# Download source of Node.JS application
+git clone https://github.com/TundraFizz/Docker-Example .
+
+# Create Docker image
+docker build -t sample-app sample-app
+
+# Build NGINX configuration files
+bash mollusk.sh nconf -c sample-app -s ip
+bash mollusk.sh nconf -c phpmyadmin -s ip -p 9000
+
+# Initialize a Docker swarm and deploy
+docker swarm init
+docker stack deploy -c docker-compose.yml sample
