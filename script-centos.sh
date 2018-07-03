a8c085c9c6b72ecdaf3658c422cb6425edb73f8c
Tundra Fizz <coleman.leif@gmail.com>
2018-07-03 12:43:21 -0700

    Update script-centos.sh

--- script-centos.sh
+++ script-centos.sh
@@ -15,27 +15,36 @@ yum -y install -y yum-utils device-mapper-persistent-data lvm2
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 yum -y install docker-ce
 
-# Create the "docker" group
-groupadd docker
-
-# Get all users
-USERS=$(cat /etc/passwd | grep "/home" | cut --delimiter=: --fields=1)
-
-# Add all users to the "docker" group
-for x in $USERS
-do
-  usermod -aG docker $x
-done
-
-# Add Google's DNS to the Docker daemon; this allows Docker containers to connect to the internet
-# NOT NEEDED?!
-# echo '{"dns": ["8.8.8.8", "8.8.4.4"]}' > /etc/docker/daemon.json
-
 # Enable docker to automatically start on boot
 systemctl enable docker
 
-# Start docker
-systemctl start docker
+# Add all users to the docker group
+USERS=$(cat /etc/passwd | grep "/home" | cut --delimiter=: --fields=1)
 
-# Restart the machine so that group permissions are applied
+# Add all users to the "docker" group
+for i in $USERS
+do
+  usermod -aG docker $i
+done
+
+# reboot to apply the changes
 reboot
+
+################################################################################
+# Steps:
+# 1. Create a new directory
+# mkdir a && cd a
+#
+# 2. Download source of Node.JS application
+# git clone https://github.com/TundraFizz/Docker-Example .
+#
+# 3. Create Docker image
+# docker build -t sample-app sample-app
+#
+# 4. Build NGINX configuration files
+# bash mollusk.sh nconf -c sample-app -s 34.218.241.246
+# bash mollusk.sh nconf -c phpmyadmin -s 34.218.241.246 -p 9000
+#
+# 5. Deploy
+# docker stack deploy -c docker-compose.yml sample
+################################################################################
