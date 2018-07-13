a4b48f1a2857d69a2dce8c0c44db6f048c3b9c7f
Tundra Fizz <MageLeif@Yahoo.com>
2018-07-13 16:38:21 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,5 +8,5 @@ bash mollusk.sh nconf -c sample-app -s ip
 bash mollusk.sh nconf -c phpmyadmin -s ip -p 9000
 
 # Initialize a Docker swarm and deploy
-docker swarm init
+# docker swarm init
 docker stack deploy -c docker-compose.yml sample
